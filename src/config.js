ebeea5655ff828c4d181ec0a93e065af793f15a2
Kiami Nichols <kiamimariah@yahoo.com>
2019-09-11 16:22:51 -0400

    tables

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-  }
+    PORT: process.env.PORT || 8080,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+    "http://localhost:3000/api"
+  };
