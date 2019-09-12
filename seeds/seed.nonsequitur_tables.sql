e0f9c8763d105e460b3b2a8d62af46f84f0b84e3
Kiami Nichols <kiamimariah@yahoo.com>
2019-09-11 20:06:29 -0400

    sprinkling more seeds

--- seeds/seed.nonsequitur_tables.sql
+++ seeds/seed.nonsequitur_tables.sql
@@ -2,5 +2,18 @@ BEGIN;
 
 INSERT INTO nonsequitur (text)
 VALUES
-('')
-  
+('Sing the national anthem over your least favorite politician debate reel. '),
+('Run backwards while yodelling, because Beyonce swears by it. '),
+('Drink a shot of pickle juice. This is helpful for cleaning your tongue.'),
+('Walk your neighbor"s dog, while they watch'),
+('Enjoy a refreshing glass of CO2 '),
+('Take the impossible quiz and cheat'),
+('Scream into the void. Its good for you'),
+('Adopt a feral cat and teach it how to play dominos'),
+('Spend the night in a graveyard and bring your ouija board as a pillow'),
+('Enjoy a night of pampering yourself with mayonaise face masks'),
+('Download and play every single ad you get for games'),
+('Watch every video recommended to you on Youtube'),
+('TIP: if you are not a smoker and want to take smoke breaks, buy a sparkler and hold it like a cigarette.'),
+('Peel and onion and let the tears flow'),
+
--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:3000/api"
+    "http://localhost:3000/api",
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DB_URL: process.env.DB_URL || 'postgresql://nonsequitur@localhost/nonsequitur',
   };
